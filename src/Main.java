0e028399e503354ba4453cb0bcaa41d9796e30bd
asegomed <asegomed@myuax.com>
2025-02-07 10:40:32 +0100

    Variables y pedir por consola

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,34 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welcome!\n");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int a = 2;
+        double b = 3.14;
+        char c = 'c';
+        String d = "hola";
+
+        System.out.printf(a + " \n" + b + " \n" + c + " \n" + d);
+
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\nIntroduzca un valor entero");
+        a = sc.nextInt();
+        System.out.println("\nEl valor entero es " + a);
+
+        if (a % 2 == 0) {
+            System.out.println("El numero "+ a + " es par");
+        }else{
+            System.out.println("El numero "+ a + " es impar");
         }
+
+
+
     }
 }
